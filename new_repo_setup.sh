4a663d7573316f934e7db91f888cc8e2f2501a91
sanram129 <sanjay.ramnauth@mail.utoronto.ca>
2025-07-17 14:11:32 -0400

    Minor changes made to requirements.txt and new_repo_setup.sh to improve setup instructions and ensure correct paths.

--- new_repo_setup.sh
+++ new_repo_setup.sh
@@ -16,30 +16,37 @@ sudo apt install -y python3-venv && \
 python3 -m venv arm_env --system-site-packages --symlinks && \
 source arm_env/bin/activate && \
 
+# Go back to the source directory and install pip requirements 
+cd src/rsx_arm && \
+pip install -r requirements.txt && \
+
 # This line reinstalls colcon-core and setuptools to ensure they use the correct Python environment.
 pip install --force-reinstall colcon-core setuptools==$(pip list --no-index --format=json |  jq -r '.[] | select(.name=="setuptools").version') && \
+
+# Go back down to workspace root
+cd ../.. && \
 
 # Build the workspace for the first time
 colcon build && \
 
+workspace_dir=$(pwd) && \
+
 # A helper command to enter the workspace
-alias workon_arm="cd $(pwd) && source /opt/ros/jazzy/setup.bash && source arm_env/bin/activate && source install/setup.bash" && \
-echo 'alias workon_arm="cd $(pwd) && source /opt/ros/jazzy/setup.bash && source arm_env/bin/activate && source install/setup.bash"' >> ~/.bashrc && \
+alias workon_arm="cd $workspace_dir && source /opt/ros/jazzy/setup.bash && source arm_env/bin/activate && source install/setup.bash" && \
+echo -e "\nalias workon_arm='cd $workspace_dir && source /opt/ros/jazzy/setup.bash && source arm_env/bin/activate && source install/setup.bash'" >> ~/.bashrc && \
 workon_arm && \
 
 # Another helper to build arm code
 alias build_arm="workon_arm && colcon build" && \
-echo 'alias build_arm="workon_arm && colcon build"' >> ~/.bashrc && \
+echo -e "\nalias build_arm='workon_arm && colcon build'" >> ~/.bashrc && \
 build_arm && \
 
 # Make sure colcon doesn't try to build the virtual environment
 touch arm_env/COLCON_IGNORE && \
 
-# Go back to the source directory and install pip requirements
-cd src/rsx_arm && \
-pip install -r requirements.txt && \
-
 # A final message to indicate completion
 echo "" && \
 echo "Setup complete. In the future, use 'workon_arm' to enter the workspace and 'build_arm' to build the packages." && \
-echo "Use the \"deactivate\" command to exit the virtual environment when done."
+echo -e "Use the \"deactivate\" command to exit the virtual environment when done." && \
+
+cd src/rsx_arm
