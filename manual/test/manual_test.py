6f9e3fdfdc19f28d49014529a4ce69ac0696b929
SarinaMashreghi <sarina.m1384@gmail.com>
2025-09-27 15:29:09 -0400

    formatting

--- manual/test/manual_test.py
+++ manual/test/manual_test.py
@@ -14,23 +14,20 @@ from std_msgs.msg import Float32MultiArray
 # TODO: move to utilities folder (can just copy past into test folers for now)
 class test_node(Node):
     def __init__(self, publishers, subscribers):
-        # input publishers and subscribers as (name, type, topic)
+        #input publishers and subscribers as (name, type, topic)
 
         super().__init__('test_node')
         self.test_publishers = {}
         for publisher in publishers:
-            self.test_publishers[publisher[0]] = self.create_publisher(
-                publisher[1], publisher[2], 10)
+            self.test_publishers[publisher[0]] = self.create_publisher(publisher[1], publisher[2], 10)
 
         self.test_subscribers = {}
         for subscriber in subscribers:
-            self.test_subscribers[subscriber[0]] = self.create_subscription(
-                subscriber[1], subscriber[2], self.listener_callback, 10)
+            self.test_subscribers[subscriber[0]] = self.create_subscription(subscriber[1], subscriber[2], self.listener_callback, 10)
 
 
 def test_sample():
-    assert 2+2 == 4
-
+    assert 2+2==4
 
 def test_manual_init():
     args = None
