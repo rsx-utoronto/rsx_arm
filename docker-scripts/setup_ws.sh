77ba1e620b34653944a628604b64d39df853d013
Kevin_KyawThiha <k.thiha10.mail@gmail.com>
2025-10-03 01:45:43 +0000

    Removed symlink for colcon build since it is not working. Also removed redudant python packages installation.

--- docker-scripts/setup_ws.sh
+++ docker-scripts/setup_ws.sh
@@ -32,18 +32,11 @@ source_relaxed "/opt/ros/${ROS_DISTRO}/setup.sh"
 
 # ----------------- Build -----------------
 echo "==> Building workspace (colcon)"
-colcon build --symlink-install \
-  --cmake-args -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON \
-  "$@"
+colcon build --cmake-args -DCMAKE_BUILD_TYPE="${BUILD_TYPE}" -DCMAKE_EXPORT_COMPILE_COMMANDS=ON "$@"
 
 # Overlay for this shell
 echo "==> Sourcing overlay: ${WS_DIR}/install/setup.sh"
 source_relaxed "${WS_DIR}/install/setup.sh"
-
-# Install the requirements
-echo "==> Install python packages: ${WS_DIR}/src/rsx_arm/requirements.txt"
-python3 -m pip install --upgrade pip setuptools wheel
-python3 -m pip install --no-cache-dir -r "${WS_DIR}/src/rsx_arm/requirements.txt"
 
 echo "Build complete at ${WS_DIR}."
 echo "Tip: open a new shell (or 'source ${WS_DIR}/install/setup.sh') to overlay your current session."
