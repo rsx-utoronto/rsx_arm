94e74ef42a70d90f4dacaf4c5678d4917bb2a9d1
Sunny Zhang <sunnyzhang403@outlook.com>
2025-11-29 14:21:16 -0500

    start yolo inference

--- auto_keyboard/auto_keyboard/camera_node.py
+++ auto_keyboard/auto_keyboard/camera_node.py
@@ -5,7 +5,8 @@ import numpy as np
 import rclpy
 from rclpy.node import Node
 import cv2
-
+import ultralytics
+from ultralytics import YOLO
 # subscribe to camera data node (sensor_msg/msg/Image.msg)
 # cv_bridge image data converted to numpy array
 
@@ -19,6 +20,12 @@ class CameraNode(Node):
         self.bridge = CvBridge()
         self.camera_sub = self.create_subscription(Image, '/camera/camera/color/image_rect_raw', self.callback, 10)
         self.last_frame = np.ndarray
+
+        # load a YOLO model
+        self.model = YOLO('yolov8n.pt')
+
+        self.get_logger().info("Camera YOLO node started.")
+
     def callback(self, data):
         try:
             # convert Image to numpy array
@@ -28,7 +35,18 @@ class CameraNode(Node):
             cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding="bgr8")
             self.last_frame = cv_image
 
-        except CvBridgeError as e:
+            # Run YOLO inference
+            results  = self.model(cv_image)
+
+            # get bounding boxes
+            annotated_frame = results[0].plot()
+            # print("heyo")
+            # Display live stream
+            cv2.imshow("YOLO Inference", annotated_frame)
+            cv2.waitKey(1)
+
+        except CvBridgeError as e: 
+            # HI DUDE
             rclpy.logerr(e)
             return
 
@@ -38,7 +56,11 @@ def main(args=None):
     try:
         rclpy.spin(camera_node)
     except KeyboardInterrupt:
+        pass
+    finally:
         camera_node.destroy_node()
+        rclpy.shutdown()
+        cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main()
