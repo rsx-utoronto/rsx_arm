df5f793192ce5c839dd5ee139ea6ac7bd9e7ab24
Sunny Zhang <sunnyzhang403@outlook.com>
2026-02-06 23:24:32 -0500

    setup.py fix

--- gui/setup.py
+++ gui/setup.py
@@ -20,7 +20,7 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'gui = gui.gui:main'
+            'arm_gui = gui.arm_gui:main'
         ],
     },
 )
