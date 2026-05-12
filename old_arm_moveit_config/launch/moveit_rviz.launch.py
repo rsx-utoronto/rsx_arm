5f05e04685ec54ffedf350ce120e521e0785d61f
sanram129 <sanjay.ramnauth@mial.utoronto.ca>
2026-05-11 21:54:43 -0400

    Revert "First new urdf commit with package. new urdf is in arm_moveit_config, old moved to old_arm_moveit_config. Still some fixes to be made with end effector (may be removed)"
    This reverts commit c60128bb35db1cdb335478b74d26cba6335dc57f.

--- old_arm_moveit_config/launch/moveit_rviz.launch.py
+++
@@ -1,8 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_moveit_rviz_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder(
-        "Arm_URDF_2025", package_name="old_arm_moveit_config").to_moveit_configs()
-    return generate_moveit_rviz_launch(moveit_config)
--- old_arm_moveit_config/launch/spawn_controllers.launch.py
+++
@@ -1,8 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_spawn_controllers_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder(
-        "Arm_URDF_2025", package_name="old_arm_moveit_config").to_moveit_configs()
-    return generate_spawn_controllers_launch(moveit_config)
